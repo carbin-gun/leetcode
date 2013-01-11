1bcf27d3ef3b38b843ee4426722e0e1ba1b84d30
keweiw <karson.wang@gmail.com>
2013-01-10 20:53:23 -0500

    second round

---
+++ leetcode/ImplementstrStr.java
@@ -0,0 +1,74 @@
+/**
+ * Implement strStr() 
+ * 
+ * The strstr() function shall locate the first occurrence in
+ * the string pointed to by s1 of the sequence of bytes (excluding the
+ * terminating null byte) in the string pointed to by s2. Implement strStr()
+ * function.
+ * 
+ * Return a pointer to the first occurrence of needle in haystack, or null if
+ * needle is not part of haystack.
+ * 
+ * Do not use any system library functions such as strlen.
+ * 
+ */
+
+/*
+ * The solution is using KMP to catch the first substring.
+ */
+public class ImplementstrStr {
+	public String strStr(String haystack, String needle) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (needle.equals("")) {
+			return haystack;
+		}
+		int[] prefix = prefix(needle.toCharArray());
+		int index = 0;
+		int pointer = 0;
+		while (index < haystack.length()) {
+			if (pointer == needle.length()) {
+				return haystack.substring(index - pointer);
+			} else {
+				if (haystack.charAt(index) == needle.charAt(pointer)) {
+					index++;
+					pointer++;
+				} else {
+					if (prefix[pointer] == -1) {
+						index++;
+						pointer = 0;
+					} else {
+						pointer = prefix[pointer];
+					}
+				}
+			}
+		}
+		if (pointer == needle.length()) {
+			return haystack.substring(haystack.length() - pointer);
+		}
+		return null;
+	}
+
+	/*
+	 * To generate the prefix array
+	 */
+	public int[] prefix(char[] pattern) {
+		int[] prefix = new int[pattern.length];
+		prefix[0] = -1;
+		if (pattern.length == 1) {
+			return prefix;
+		}
+		prefix[1] = 0;
+		int j = 0;
+		for (int i = 1; i < pattern.length - 1; i++) {
+			if (pattern[i] == pattern[j]) {
+				prefix[i + 1] = prefix[i] + 1;
+				j++;
+			} else {
+				prefix[i + 1] = 0;
+				j = 0;
+			}
+		}
+		return prefix;
+	}
+}
--- leetcode/InsertInterval.java
+++ leetcode/InsertInterval.java
@@ -1,14 +1,28 @@
-package unsorted;
-
 import java.util.ArrayList;
 
 /**
- * Insert Interval This is a little bit skillful, every one know the question,
- * but hard to write down a smart solution.
+ * Insert Interval
  * 
+ * Given a set of non-overlapping intervals, insert a new interval into the
+ * intervals (merge if necessary).
+ * 
+ * You may assume that the intervals were initially sorted according to their
+ * start times.
+ * 
+ * Example 1: Given intervals [1,3],[6,9], insert and merge [2,5] in as
+ * [1,5],[6,9].
+ * 
+ * Example 2: Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in
+ * as [1,2],[3,10],[12,16].
+ * 
+ * This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10]. *
+ */
+
+/*
+ * Find the intervals will be conflicted with the insert interval, remove them 
+ * and insert a new combine all.
  */
 public class InsertInterval {
-
 	public ArrayList<Interval> insert(ArrayList<Interval> intervals,
 			Interval newInterval) {
 		// Start typing your Java solution below
@@ -34,7 +48,6 @@ public class InsertInterval {
 			int end = Math.max(intervals.get(e).end, newInterval.end);
 			intervals.subList(s, e + 1).clear();
 			intervals.add(s, new Interval(start, end));
-
 		}
 		return intervals;
 	}
